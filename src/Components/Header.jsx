e9351b094ebcf91f62fdc5ed25106601e0d4ea31
kalixer <al.caceres.ag72@gmail.com>
2024-02-29 12:11:57 -0300

    [Mod] Modded the header adding an array that returns each button. Also created the pages to go when you click the buttons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Header.scss';
 import luthorLogo from '../assets/luthorLogo.png';
 
 function Header() {
+  const sections = [
+    {
+      title: 'Home',
+      path: '/'  
+    },
+    {
+      title: 'Ingeniería y Cálculo',
+      path: '/engineering'  
+    },
+    {
+      title: 'Construcción',
+      path: '/construction'  
+    },
+    {
+      title: 'Blog',
+      path: '/#'  
+    },
+    {
+      title: 'Contacto',
+      path: '/#' 
+    },
+]
+
   return (
     <div className='Header'>
       <figure className='logo'>
         <img src={luthorLogo} alt="" />
       </figure>
       <li>
-        <ul>Ingeniería y Cálculo</ul>
-        <ul>Construcción</ul>
-        <ul>Casas</ul>
-        <ul>Ampliaciones</ul>
+        {sections.map(item => (
+          <Link to={item.path}>
+            <ul>{item.title}</ul>
+          </Link>
+        ))}
       </li>
     </div>
   )
--- src/Pages/EngineeringPage.jsx
+++ src/Pages/EngineeringPage.jsx
@@ -4,7 +4,7 @@ import { IntroPicture } from '../Components/IntroPicture'
 
 function EngineeringPage() {
   const image = 'https://images.pexels.com/photos/110469/pexels-photo-110469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  const title = 'Servicios de cálculo estructural e ingeniería civil'
+  const title = 'Solve Ingeniería y Cálculo'
   const titleDesc = 'Servicios de cálculo estructural e ingeniería civil'
   return (
     <>
