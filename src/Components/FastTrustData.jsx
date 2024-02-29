03645bf788a3d6a18d0e0058e942d7e01d603f94
kalixer <al.caceres.ag72@gmail.com>
2024-02-28 23:37:00 -0300

    [Add] I added the component 'FastTrustData'. I named it that way cuz I didn't know how to name it and it's made to build trust

---
+++ src/Components/FastTrustData.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../styles/FastTrustData.scss'
+
+import { TfiHeadphoneAlt } from "react-icons/tfi";
+
+function FastTrustData() {
+  const texto = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis cupiditate ad nesciunt `
+  return (
+    <div className='FastTrustData'>
+      <li className='fast-container'>
+        <ul className='fast-block'>
+          <TfiHeadphoneAlt/>
+          <h3>Title</h3>
+          <p>{texto}</p>
+        </ul>
+        <ul className='fast-block'>
+          <TfiHeadphoneAlt/>
+          <h3>Title</h3>
+          <p>{texto}</p>
+        </ul>
+        <ul className='fast-block'>
+          <TfiHeadphoneAlt/>
+          <h3>Title</h3>
+          <p>{texto}</p>
+        </ul>
+        <ul className='fast-block'>
+          <TfiHeadphoneAlt/>
+          <h3>Title</h3>
+          <p>{texto}</p>
+        </ul>
+      </li>
+    </div>
+  )
+}
+
+export { FastTrustData }
