d3cb2384e82775cdb8d312776b51d5089fe25f33
kalixer <al.caceres.ag72@gmail.com>
2024-03-04 12:17:54 -0300

    [Add] Mockup finished on Pros.jsx

--- src/Pages/EngineeringPage/Pros.jsx
+++ src/Pages/EngineeringPage/Pros.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './Pros.scss'
+import { AiOutlineExperiment } from "react-icons/ai";
 
 function Pros() {
+  const adv_desc = 'Lideres en la era de la construcción de no se qué porque este texto es solo como ejemplo'
+
   return (
-    <div className='Pros'>Pros</div>
+    <div className='Pros'>
+
+      <div className='title'>
+        <h1>Ventajas</h1>
+      </div>
+
+      
+      <div className='advantageBox'>
+
+        <div className='box'>
+          <AiOutlineExperiment />
+          <h3>Ventaja X</h3>
+          <p>{adv_desc}</p>
+        </div>
+        
+        <div className='box'>
+          <AiOutlineExperiment />
+          <h3>Ventaja X</h3>
+          <p>{adv_desc}</p>
+        </div>
+        
+        <div className='box'>
+          <AiOutlineExperiment />
+          <h3>Ventaja X</h3>
+          <p>{adv_desc}</p>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
