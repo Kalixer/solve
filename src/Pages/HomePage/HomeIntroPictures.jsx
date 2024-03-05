86a1cca1a7e928f5b34e19f8f4eae10bc39bb627
kalixer <al.caceres.ag72@gmail.com>
2024-03-05 15:29:56 -0300

    [Mod] The component 'HomeIntroPictures' displays with a diagonal polygon

--- src/Pages/HomePage/HomeIntroPictures.jsx
+++ src/Pages/HomePage/HomeIntroPictures.jsx
@@ -8,18 +8,21 @@ function HomeIntroPictures() {
     return (
     <>
         <div className='HomeIntroPictures'>
+
             <div className='pictureBox'>
                 <figure>
                     <img src={calcImage} alt="" />
                 </figure>
                 <h1>Ingeniería y Cálculo</h1>
             </div>
+
             <div className='pictureBox'>
                 <figure>
                     <img src={houseImage} alt="" />
                 </figure>
                 <h1>Diseño y Construcción</h1>
             </div>
+            
         </div>
     </>
   )
