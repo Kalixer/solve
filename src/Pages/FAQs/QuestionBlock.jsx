c064cb0f0e062edc4b276b3ed75cb71c8c8560ba
kalixer <al.caceres.ag72@gmail.com>
2024-03-06 13:49:20 -0300

    [Add] I added a toggle button instread of the ugly 'V'. It rotates when you click on it and it changes the color onMouseOver

--- src/Pages/FAQs/QuestionBlock.jsx
+++ src/Pages/FAQs/QuestionBlock.jsx
@@ -1,21 +1,41 @@
 import React from 'react'
 import './QuestionBlock.scss';
 
+import { IoIosArrowForward } from "react-icons/io";
+
 function QuestionBlock() {
     const [answerDis, setAnswerDis] = React.useState(false)
+    const [rotate, setRotate] = React.useState(false)
+    const [isOver, setIsOver] = React.useState(false)
 
     const toggleAns = () => {
         setAnswerDis(!answerDis)
+        setRotate(!rotate)
+        // button.classList.toggle('rotate')
     }
 
+    const question = 'Pregunta ejemplo para quien quiere preguntar una pregunta'
     const answer = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, accusantium dicta, cum ipsa iusto odio praesentium dolores in laudantium eveniet omnis ut doloribus quisquam a et, sequi harum soluta temporibus?'
 
 
   return (
     <div className='QuestionBlock'>
         <div className='question'>
-            <h3>Pregunta ejemplo para quien quiere preguntar una pregunta</h3>
-            <button onClick={toggleAns}>V</button>
+            <h3>{question}</h3>
+
+            <button 
+                className={rotate ? 'rotate' : ''}
+                onClick={toggleAns}
+                onMouseOver={() => setIsOver(true)}
+                onMouseLeave={() => setIsOver(false)}
+            >
+                {
+                    isOver ? 
+                    <IoIosArrowForward fill='black'/>
+                    : <IoIosArrowForward fill='grey'/>
+                }
+            </button>
+            
         </div>
         
         {answerDis 
