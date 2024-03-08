9401a13b8e06965943ff691caab85e00ddc2187d
kalixer <al.caceres.ag72@gmail.com>
2024-03-08 19:33:58 -0300

    [Mod] I changed the location of the 'CategoryBlock' to the ConstructionPage. I also changed the position of the component 'FastTrustData' between Slove Ingeniería y Solve Construcción

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -3,7 +3,6 @@ import './index.scss';
 import { HomeIntroPictures } from './HomeIntroPictures';
 import { AboutUs } from './AboutUs';
 import { BarrData } from './BarrData';
-import { CategoryBlocks } from './CategoryBlocks';
 import { FastTrustData } from './FastTrustData';
 import { PresentationIng } from './PresentationIng';
 import { PresentationConst } from './PresentationConst';
@@ -13,11 +12,10 @@ function Home() {
   return (
     <div className='Home'>
         <HomeIntroPictures/>
-        <FastTrustData/>
         <AboutUs/>
         <BarrData/>
         <PresentationIng/>
-        <CategoryBlocks/>
+        <FastTrustData/>
         <PresentationConst/>
         <LetsConect/>
     </div>
