84f5d4a9abcf77cd862956439822f1a2e73a7806
kalixer <al.caceres.ag72@gmail.com>
2024-03-11 17:23:27 -0300

    [Add & Mod] Buttons added and padding fixed on the description

--- src/Pages/ProjectsPage/index.jsx
+++ src/Pages/ProjectsPage/index.jsx
@@ -3,6 +3,9 @@ import './index.scss'
 import { ProjectData } from './ProjectData'
 import { Description } from './Description'
 
+import { SlArrowLeft } from "react-icons/sl";
+import { SlArrowRight } from "react-icons/sl";
+
 function ProjectsPage() {
     const imagen = 'https://images.pexels.com/photos/2089698/pexels-photo-2089698.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1'
   return (
@@ -10,9 +13,12 @@ function ProjectsPage() {
         <figure className='imagePage'>
             <img src={imagen} alt="" />
         </figure>
+
+        <button className='boton botonAnterior'><SlArrowLeft fill='white'/></button>
+        <button className='boton botonSiguiente'><SlArrowRight fill='white' /></button>
+
         <Description/>
         <ProjectData/>
-
     </div>
   )
 }
