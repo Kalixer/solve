ec9b2bad6dd866370dcecd5f44737208c9a99c04
kalixer <al.caceres.ag72@gmail.com>
2024-03-17 18:21:34 -0300

    [responsive fixes on homepage]

--- src/Pages/HomePage/PresentationConst.jsx
+++ src/Pages/HomePage/PresentationConst.jsx
@@ -7,7 +7,24 @@ import './PresentationConst.scss'
 
 function PresentationConst() {
   const image = 'https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1'
-  const desc = 'Con una amplia gama de opciones, ofrecemos modelos de casas tanto en uno como en dos pisos para adaptarse a sus necesidades. Nuestros servicios incluyen la elaboración de presupuestos para construcción inmediata y la modalidad de entrega "Construcción Llave en Mano", asegurando comodidad y eficiencia. Especializados en proyectos habitacionales y de especialidades, nos comprometemos a ofrecer soluciones integrales para su proyecto, desde el diseño hasta la entrega final. Confíe en nosotros para convertir su visión en realidad.'
+  const desc = (
+    <>
+    
+      <p> 
+        Con una amplia gama de opciones, ofrecemos modelos de casas tanto en uno como en dos pisos para adaptarse a sus necesidades.
+      </p> 
+      <p>
+        Nuestros servicios incluyen la elaboración de presupuestos para construcción inmediata y la modalidad de entrega "Construcción Llave en Mano", asegurando comodidad y eficiencia.
+      </p>
+      <p>
+        Especializados en proyectos habitacionales y de especialidades, nos comprometemos a ofrecer soluciones integrales para su proyecto, desde el diseño hasta la entrega final.
+      </p> 
+      <p>
+        Confíe en nosotros para convertir su visión en realidad.
+      </p>
+      
+    </>
+  )
   
   return (
     <div className='PresentationConst'>
@@ -15,7 +32,7 @@ function PresentationConst() {
       <section className='constDescription'>
         <div className='textContainer'>
           <h1>Solve Construcción</h1>
-          <p>{desc}</p>
+          {desc}
           <Link to='/contact' onClick={scrollToTop}>
             <button type='button' className='hablemos'>Hablemos</button>
           </Link>
