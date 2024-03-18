aa9168e0b9eb682129314728a98e126998aec119
kalixer <al.caceres.ag72@gmail.com>
2024-03-18 16:05:29 -0300

    [Mod] Const created and correct spacing tab modified

--- src/Pages/HomePage/AboutUs.jsx
+++ src/Pages/HomePage/AboutUs.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import './AboutUs.scss';
 import houseInteriorVertical from '../../assets/houseInteriorVertical.jpg'
-function AboutUs() {
-  return (
-    <div className='AboutUs'>
-        <section className='contentDescript'>
-            <div className='text'>
-                <p>About Us</p>
-                <h1>Title of Description</h1>
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ullam architecto vel vero nesciunt ex eligendi, excepturi molestiae dolor exercitationem asperiores adipisci minima recusandae voluptatem obcaecati quisquam quos maxime ut!</h3>
-            </div>
-            <div className='boton'>
-                <button type='button'>Click me!</button>
-            </div>
-        </section>
 
-        <section className='imagePresentation'>
-            <figure>
-                <img src={houseInteriorVertical} alt="" />
-            </figure>
-        </section>
-    </div>
-  )
+function AboutUs() {
+    const about = ''
+    return (
+      <div className='AboutUs'>
+          <section className='contentDescript'>
+              <div className='text'>
+                  <p>About Us</p>
+                  <h1>Title of Description</h1>
+                  <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ullam architecto vel vero nesciunt ex eligendi, excepturi molestiae dolor exercitationem asperiores adipisci minima recusandae voluptatem obcaecati quisquam quos maxime ut!</h3>
+              </div>
+              <div className='boton'>
+                  <button type='button'>Click me!</button>
+              </div>
+          </section>
+
+          <section className='imagePresentation'>
+              <figure>
+                  <img src={houseInteriorVertical} alt="" />
+              </figure>
+          </section>
+      </div>
+    )
 }
 
 export { AboutUs }
