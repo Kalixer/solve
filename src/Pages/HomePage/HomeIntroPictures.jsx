7167abdf415e9f08f9dd5dc6f376474bcbedfdc2
kalixer <al.caceres.ag72@gmail.com>
2024-05-07 13:37:48 -0400

    [Add Language changer => HomeIntroPictures]

--- src/Pages/HomePage/HomeIntroPictures.jsx
+++ src/Pages/HomePage/HomeIntroPictures.jsx
@@ -1,16 +1,25 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom'
 // import calcImage from '../../assets/calculus2Vertical.jpg'
 import houseImage from '../../assets/houseImageVertical.jpg'
 
 import { scrollToTop } from '../../utils/scrollToTop';
+import { MyContext } from '../../hooks/MyContext';
+import { componentLang } from './HomeIntroPicturesLang';
 
 import './HomeIntroPictures.scss'
 
 function HomeIntroPictures() {
+
+    const { languageEs } = useContext(MyContext)
+
     const calcSrc = 'https://images.pexels.com/photos/5584052/pexels-photo-5584052.jpeg?auto=compress&cs=tinysrgb&w=1920&h=2880&dpr=1' 
 
-    const ingDesc = 'Nos especializamos en el cálculo estructural con años de experiencia'
-    const constDesc = 'La construcción de casas está respaldada por la experiencia y asesoramiento de un ingeniero civil calculista'
+    const ingDesc_Es = componentLang.ingDesc_Es
+    const ingDess_En = componentLang.ingDesc_En
+    const constDesc_Es = componentLang.constDesc_Es
+    const constDesc_En = componentLang.constDesc_En
+    
     return (
     <>
         <div className='HomeIntroPictures'>
@@ -20,10 +29,10 @@ function HomeIntroPictures() {
                     <img src={calcSrc} alt="" />
                 </figure>
                 <div className='texto'>
-                    <h1>Solve Ingeniería: Cálculo estructural</h1>
-                    <p>{ingDesc}</p>
+                    <h1>{languageEs ? componentLang.solveEngineering_Es : componentLang.solveEngineering_En}</h1>
+                    <p>{languageEs ? ingDesc_Es : ingDess_En}</p>
                     <Link className='b_link' to='/engineering' onClick={scrollToTop}>
-                        <button>Quiero saber más</button>
+                        <button>{languageEs ? componentLang.contact_Es : componentLang.contact_En}</button>
                     </Link>
                 </div>
             </div>
@@ -33,10 +42,10 @@ function HomeIntroPictures() {
                     <img src={houseImage} alt="" />
                 </figure>
                 <div className='texto'>
-                    <h1>Solve Construcción</h1>
-                    <p>{constDesc}</p>
+                    <h1>{languageEs ? componentLang.solveConstruction_Es : componentLang.solveConstruction_En}</h1>
+                    <p>{languageEs ? constDesc_Es : constDesc_En}</p>
                     <Link to='/construction' onClick={scrollToTop} className='b_link'>
-                        <button>Quiero saber más</button>
+                        <button>{languageEs ? componentLang.contact_Es : componentLang.contact_En}</button>
                     </Link>
                 </div>
             </div>
---
+++ src/Pages/HomePage/HomeIntroPicturesLang.js
@@ -0,0 +1,12 @@
+export const componentLang = {
+    solveEngineering_Es: 'Solve Ingeniería: Cálculo estructural',
+    solveEngineering_En: 'Solve Engineering: Structural calculation',
+    ingDesc_Es: 'Somos especialistas en el cálculo estructural con años de experiencia',
+    ingDesc_En: 'We specialize in structural calculations with years of experience',
+    constDesc_Es: 'La construcción de casas está respaldada por la experiencia y asesoramiento de un ingeniero civil calculista',
+    constDesc_En: 'The construction of houses is supported by the experience and advice of a civil calculating engineer',
+    contact_Es: 'Quiero saber más',
+    contact_En: 'I want to know more',
+    solveConstruction_Es: 'Solve Construcción',
+    solveConstruction_En: 'Solve Construction',
+}
